3ada6b61fa47b43f2fdf20c6a967416f57ec10cd
Corey Clark <coreyclark@Coreys-MacBook-Air.local>
2016-05-09 09:27:32 -0700

    Adds test libs

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var pj = require('./index');
+var expect = require('chai').expect;
+
+describe('All methods', function() { 
+    it('should work', function() { 
+        expect(true).to.be.true; 
+    });
+});
