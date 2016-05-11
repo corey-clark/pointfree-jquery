97524a29d6cda97b7559838d7c590fd7c5c1c383
Corey Clark <coreyclark@Coreys-MacBook-Air.local>
2016-05-10 21:08:54 -0700

    Adds event handlers

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,28 @@
 'use strict';
 
+var trace = function trace(msg) {
+    return function (x) {
+        console.log(msg, x);
+        return x;
+    };
+};
+
+var utils = {
+    trace: trace
+};
+
 var effectsList = ['fadeIn', 'fadeOut', 'fadeTo', 'fadeToggle', 'hide', 'show', 'slideToggle', 'slideUp', 'toggle'];
+
+var handlersList = ['bind', 'blur', 'change', 'click', 'contextmenu', 'dblclick', 'die', 'focus', 'focusout', 'keydown', 'keypress', 'keyup', 'live', 'load', 'mousedown', 'mouseenter', 'mouseleave', 'mousemove', 'mouseout', 'mouseover', 'mouseup', 'on', 'resize', 'scroll', 'select', 'submit', 'unbind', 'unload'];
+
+var buildHandlers = function buildHandlers(acc, x) {
+    acc[x] = function (evt, cb) {
+        return function (elem) {
+            return elem[x](evt, cb);
+        };
+    };
+    return acc;
+};
 
 var buildEffects = function buildEffects(acc, x) {
     acc[x] = function (cb) {
@@ -12,5 +34,9 @@ var buildEffects = function buildEffects(acc, x) {
 };
 
 var effects = effectsList.reduce(buildEffects, {});
+var handlers = handlersList.reduce(buildHandlers, {});
 
-module.exports = effects;
+var api = Object.assign({}, effects, handlers, utils);
+
+console.log(api);
+module.exports = api;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const trace = msg => x => { 
+    console.log(msg, x);
+    return x;
+};
+
+const utils = { 
+    trace
+};
+    
 const effectsList = [ 
     'fadeIn',
     'fadeOut',
@@ -10,11 +19,53 @@ const effectsList = [
     'toggle'
 ];
 
+const handlersList = [ 
+    'bind',
+    'blur',
+    'change',
+    'click',
+    'contextmenu',
+    'dblclick',
+    'die',
+    'focus',
+    'focusout',
+    'keydown',
+    'keypress',
+    'keyup',
+    'live',
+    'load',
+    'mousedown',
+    'mouseenter',
+    'mouseleave',
+    'mousemove',
+    'mouseout',
+    'mouseover',
+    'mouseup',
+    'on',
+    'resize',
+    'scroll',
+    'select',
+    'submit',
+    'unbind',
+    'unload'
+];
+
+
+const buildHandlers = (acc, x) => { 
+    acc[x] = (evt, cb) => elem => elem[x](evt, cb);
+    return acc;
+};
+
 const buildEffects = (acc, x) => { 
     acc[x] = cb => elem => elem[x](cb);
     return acc;
 };
 
 const effects = effectsList.reduce(buildEffects, {});
+const handlers = handlersList.reduce(buildHandlers, {});
 
-module.exports = effects;
+const api = Object.assign({}, effects, handlers, utils);
+
+module.exports = api;
+
+
